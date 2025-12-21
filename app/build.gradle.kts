bc1ed404dbd15b21c8dd9feebdd5359343599e1b
daviidsaanz <davidsanzmajolero@gmail.com>
2025-12-21 22:04:06 +0100

    basic view one movie

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,8 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+
+    implementation("androidx.navigation:navigation-compose:2.7.5")
 }
 
 kapt {
